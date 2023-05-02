645fedefcc7386228b1c538e665b692bd519a4ec
TaeHyun <kt9811@naver.com>
2023-05-02 14:58:34 +0900

    fila

--- dist/jquery.mousewheel.min.f95e68c8.js
+++ dist/jquery.mousewheel.min.f95e68c8.js
@@ -223,7 +223,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57218" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59315" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
