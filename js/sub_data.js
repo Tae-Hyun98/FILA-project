3248213873b75d1067e1fbe51d304fbf70d13843
TaeHyun <kt9811@naver.com>
2023-05-09 15:35:32 +0900

    fila

--- js/sub_data.js
+++ js/sub_data.js
@@ -46,6 +46,86 @@ const subData = [
     price: "32,000",
     src: "../img/Sub_img/06.jpg"
   },
+
+  {
+    id: "data07",
+    category: "공용",
+    name: "오버핏 코쿤 볼 패딩",
+    price: "139,000",
+    src: "../img/Sub_img/07.jpg"
+  },
+
+  {
+    id: "data08",
+    category: "공용",
+    name: "스몰로고 맨투맨",
+    price: "59,000",
+    src: "../img/Sub_img/08.jpg"
+  },
+
+  {
+    id: "data09",
+    category: "공용",
+    name: "헤리티지 로고 후드",
+    price: "69,000",
+    src: "../img/Sub_img/09.jpg"
+  },
+
+  {
+    id: "data10",
+    category: "공용 레귤러핏 러닝",
+    name: "FILA SPORTS 후디",
+    price: "69,000",
+    src: "../img/Sub_img/10.jpg"
+  },
+
+  {
+    id: "data11",
+    category: "공용",
+    name: "FILATECH 아노락 하프 집업",
+    price: "99,000",
+    src: "../img/Sub_img/11.jpg"
+  },
+
+  {
+    id: "data12",
+    category: "공용",
+    name: "노카라 퀄팅 자켓",
+    price: "89,000",
+    src: "../img/Sub_img/12.jpg"
+  },
+
+  {
+    id: "data13",
+    category: "공용",
+    name: "스몰로고 맨투맨",
+    price: "59,000",
+    src: "../img/Sub_img/13.jpg"
+  },
+
+  {
+    id: "data14",
+    category: "공용 라이프스타일",
+    name: "컴포트핏 인터내셔널 와이드팬츠",
+    price: "89,000",
+    src: "../img/Sub_img/14.jpg"
+  },
+
+  {
+    id: "data15",
+    category: "공용",
+    name: "리버시블 후드 플리스",
+    price: "109,000",
+    src: "../img/Sub_img/15.jpg"
+  },
+
+  {
+    id: "data16",
+    category: "공용",
+    name: "브이 배색 경량 자켓",
+    price: "119,000",
+    src: "../img/Sub_img/16.jpg"
+  },
 ]
 
 export default subData;
--- js/subpage.js
+++ js/subpage.js
@@ -1,17 +1,85 @@
 import subData from "./sub_data.js";
 
 const subProduct = document.querySelector('.list_wrap');
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 2; i++) {
   for (let j = 0; j < subData.length; j++) {
+    //서브 상품박스
     const subDiv = document.createElement('div');
     subDiv.setAttribute('class', 'product_box');
 
+    const subImgBox = document.createElement('div');
+    subImgBox.setAttribute('class', 'product_img');
+
+    const imgLink = document.createElement('a');
+    imgLink.setAttribute('href', "#!");
+    subImgBox.appendChild(imgLink);
+
     const subImg = document.createElement('img');
     subImg.setAttribute('src', subData[j].src);
+    imgLink.appendChild(subImg);
 
     const detailDiv = document.createElement('div');
+    detailDiv.setAttribute('class', 'detail');
 
-    subDiv.appendChild(subImg);
+    const detailLink = document.createElement('a');
+    detailLink.setAttribute('href', '#!');
+    detailDiv.appendChild(detailLink);
+
+    //상품박스 호버했을때
+    const hoverDiv = document.createElement('div');
+    hoverDiv.setAttribute('class', 'hover_box');
+    subImgBox.appendChild(hoverDiv);
+
+    const cartDiv = document.createElement('div');
+    hoverDiv.appendChild(cartDiv);
+    const cartSpan = document.createElement('span');
+    cartSpan.setAttribute('class', 'material-icons-outlined');
+    const cartText = document.createTextNode('shopping_cart');
+    cartDiv.appendChild(cartSpan);
+    cartSpan.appendChild(cartText);
+
+    const likeDiv = document.createElement('div');
+    hoverDiv.appendChild(likeDiv);
+    const likeSpan = document.createElement('span');
+    likeSpan.setAttribute('class', 'material-icons-outlined');
+    const likeText = document.createTextNode('favorite');
+    likeDiv.appendChild(likeSpan);
+    likeSpan.appendChild(likeText);
+
+    //카테고리
+    const cateGory = document.createElement('p');
+    cateGory.setAttribute('class', 'category');
+    detailLink.appendChild(cateGory);
+    const cateGoryText = document.createTextNode(subData[j].category);
+    cateGory.appendChild(cateGoryText);
+
+    //상품이름
+    const productName = document.createElement('p');
+    productName.setAttribute('class', 'product_name');
+    detailLink.appendChild(productName);
+    const nameText = document.createTextNode(subData[j].name);
+    productName.appendChild(nameText);
+
+    //상품가격
+    const productPrice = document.createElement('p');
+    productPrice.setAttribute('class', 'product_price');
+    detailLink.appendChild(productPrice);
+    const namePrice = document.createTextNode(subData[j].price+'원');
+    productPrice.appendChild(namePrice);
+
+    //BUY버튼
+    const buyDiv = document.createElement('div');
+    buyDiv.setAttribute('class', 'buy_btn');
+    detailDiv.appendChild(buyDiv);
+
+    const buyA = document.createElement('a');
+    buyA.setAttribute('href', '#!');
+    buyDiv.appendChild(buyA);
+
+    const buyText = document.createTextNode('BUY');
+    buyA.appendChild(buyText);
+
+    subDiv.appendChild(subImgBox);
     subDiv.appendChild(detailDiv);
     subProduct.appendChild(subDiv);
   }
